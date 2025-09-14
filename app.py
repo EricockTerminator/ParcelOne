a0d6d098cfd7c43b6036d2b0f51e298e1e7f5744
EricockTerminator <krhliak02@gmail.com>
2025-09-14 14:33:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+diff --git a//dev/null b/app.py
+index 0000000000000000000000000000000000000000..748ca714eb5ac4de9ceb923a27646c28429319aa 100644
+--- a//dev/null
++++ b/app.py
+@@ -0,0 +1,4 @@
++from parcelone.ui import main
++
++if __name__ == "__main__":
++    main()
